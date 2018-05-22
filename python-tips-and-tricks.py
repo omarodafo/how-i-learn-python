14263d8a3ac2c1b45a60f83bee0b68b93caa1f06
unknown <omarodafo@gmail.com>
2018-05-22 20:34:25 +0300

    python-tips and tricks added

---
+++ python-tips-and-tricks.py
@@ -0,0 +1,16 @@
+#Reversing a string in python
+text = "Televizyon Öldüren Eğlence"
+reversed_text = text[::-1]
+print(reversed_text)
+
+#Swap two variable
+a = 10
+b = "text"
+a,b = b,a
+print(a, b)
+
+#Create one string with all elements
+a = ["merhaba", "nasılsın"]
+b = {"merhaba","nasılsın"}
+c = " ".join(b)
+print(c)
